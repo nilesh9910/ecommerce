5cd5e12b67c07e25b77bd152bd4dc7dcad14fef5
nilesh9910 <nileshkumar998@gmail.com>
2025-02-24 02:12:04 +0530

    added elastic search functionality

--- productCatalog/src/main/java/com/example/ecommerce/services/IProductService.java
+++ productCatalog/src/main/java/com/example/ecommerce/services/IProductService.java
@@ -1,9 +1,17 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.models.ESProduct;
 import com.example.ecommerce.models.Product;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.util.List;
+
 @Repository
 public interface IProductService {
+    List<Product> getAllProducts();
     Product createProduct(Product product);
+    Product updateProduct(Product product);
+    List<ESProduct> getProductByName(String name);
+    List<Product> searchProduct(String search) throws IOException;
 }
